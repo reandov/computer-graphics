be45f2a733c24efde586608ef307a983c80e6a63
Evandro Rodrigues <evandro.rodriguespj@gmail.com>
2021-02-04 12:58:24 -0300

    adding LICENSE and first files

---
+++ manual_src/src/hello_world.cpp
@@ -0,0 +1,47 @@
+#include <windows.h>
+#include "GL/glut.h"
+
+#define RESOLUTION_HEIGHT 640
+#define RESOLUTION_WIDTH 480
+
+void init();
+void draw();
+
+int main(int argc, char** argv) {
+    
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_DEPTH | GLUT_RGBA | GLUT_ALPHA);
+    glutInitWindowSize(RESOLUTION_HEIGHT, RESOLUTION_WIDTH);
+    glutInitWindowPosition((glutGet(GLUT_SCREEN_WIDTH) - RESOLUTION_HEIGHT) / 2,
+                           (glutGet(GLUT_SCREEN_HEIGHT) - RESOLUTION_WIDTH) / 2);
+    glutCreateWindow("Hello, World!");
+
+    init();
+    glutDisplayFunc(draw);
+
+    glutMainLoop();
+
+    return 0;
+}
+
+void init() {
+    glClearColor(0, 0, 0, 0);
+}
+
+void draw() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-10, 10, -10, 10, -1, 1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    glBegin(GL_TRIANGLES);
+        glColor3f(1, 0, 0); glVertex2f(-7,-7 ); /* Bottom      */
+        glColor3f(0, 1, 0); glVertex2f( 0, 7 ); /* Upper Left  */
+        glColor3f(0, 0, 1); glVertex2f( 7,-7 ); /* Upper Right */
+    glEnd();
+
+    glFlush(); 
+}
---
+++ manual_src/src/primitives_introduction.cpp
@@ -0,0 +1,100 @@
+#include <windows.h>
+#include <math.h>
+#include "GL/glut.h"
+
+void init();
+void draw();
+void idle();
+void keyboard(unsigned char key, int x, int y);
+
+float initial_angle = M_PI / 6.0;
+float velocity = 1;
+
+int main(int argc, char** argv) {
+    
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGBA | GLUT_ALPHA);
+    glutInitWindowSize(600, 600);
+    glutInitWindowPosition((glutGet(GLUT_SCREEN_WIDTH) - 600) / 2,
+                           (glutGet(GLUT_SCREEN_HEIGHT) - 600) / 2);
+    glutCreateWindow("Primitives");
+
+    init();
+    glutKeyboardFunc(keyboard);
+    glutIdleFunc(idle);
+    glutDisplayFunc(draw);
+
+    glutMainLoop();
+
+    return 0;
+}
+
+void init() {
+    glClearColor(0, 0, 0, 0);
+}
+
+void keyboard(unsigned char key, int x, int y) {
+    switch (key) {
+        case 'v':
+            velocity = velocity - 0.1;
+            break;
+        
+        case 'V':
+            velocity = velocity + 0.1;
+            break;
+
+        case 'I':
+            velocity = velocity * (-1);
+            break;
+            
+        default:
+            break;
+    }
+}
+
+void idle() {
+    initial_angle += velocity * ((2.0 * M_PI) / 360.0);
+
+    if (initial_angle > 2 * M_PI) initial_angle -= 2 * M_PI;
+
+    glutPostRedisplay();
+}
+
+void draw_circulo(int n, float radius) {
+    
+    float delta = (2.0 * M_PI) / n;
+
+    for (int i = 0; i < n; i++) {
+        float angulo = initial_angle + i * delta;
+        float x = radius * cos(angulo);
+        float y = radius * sin(angulo);
+
+        glVertex2f(x, y);
+    }
+}
+
+void draw() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-25, 25, -25, 25, -1, 1);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    // GL_POINTS            = pontos
+    // GL_LINES             = segmentos de reta de dois em dois
+    // GL_LINE_STRIP        = uma linha conectada
+    // GL_LINE_LOOP         = uma linha conectada fechada
+    // GL_TRIANGLES         = triangulos
+    // GL_TRIANGLE_STRIP    = triangulos alternados fechando outros
+    // GL_TRIANGLE_FAN      = leque de triangulos
+    // GL_POLYGON           = polígono CONVEXO
+
+    glBegin(GL_POLYGON);
+        draw_circulo(8, 25);
+    glEnd();
+
+    //glFlush(); // DESENHA UM BUFFER
+    glutSwapBuffers(); // ALTERNA ENTRE DOIS BUFFERS
+}
